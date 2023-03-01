4cf540c8430ffe03fd95f35c1fe5d3144af97823
Hanna.Lytvynenko <ani-k-a@ukr.net>
2023-03-01 20:46:08 +0100

    add 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
 
 console.log(galleryItems);
+
+const galleryImg = document.querySelector('.gallery');
+
+const imagesBlock = galleryItems.map(({ preview, original, description }) => {
+    const image = `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`
+    return image;
+}).join("");
+
+galleryImg.insertAdjacentHTML("beforeend", imagesBlock);
+
+galleryImg.addEventListener('click', handleClick);
+
+function handleClick (event){
+  event.preventDefault();
+  new SimpleLightbox('.gallery a', {captions: true,
+    captionSelector: 'img',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250});
+} 
